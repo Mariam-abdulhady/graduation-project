db8ff3e9042ba954658498ad7a607795e9a6332a
Mariam abdulhady <132626579+Mariam-abdulhady@users.noreply.github.com>
2023-07-02 14:37:59 +0300

    Update Diagnostic form.model.js

--- models/Diagnostic form.model.js
+++ models/Diagnostic form.model.js
@@ -1,6 +1,7 @@
 const mongoose =require('mongoose')
    const DiagnosticForm =mongoose.Schema({
    path:String,
-  userID:mongoose.Schema.Types.ObjectId
+  userID:mongoose.Schema.Types.ObjectId,
+ imageID:mongoose.Schema.Types.ObjectId
 })
 module.exports=mongoose.model('formUpload',DiagnosticForm)
