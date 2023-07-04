20e31d93786fb60b12c52a515b87e83ca0d613f0
Mariam abdulhady <132626579+Mariam-abdulhady@users.noreply.github.com>
2023-07-04 17:02:55 +0300

    Update uploaded.routes.js

--- routes/uploaded.routes.js
+++ routes/uploaded.routes.js
@@ -1,8 +1,8 @@
 'use strict';
 const app=require('express').Router()
 const diagonsticForm=require('../models/Diagnostic form.model')
-const auth = require('../middelware/auth')
-const jwt = require('jsonwebtoken');
+//const auth = require('../middelware/auth')
+//const jwt = require('jsonwebtoken');
 var multer = require('multer');
 //the image will be shown with its name when we transfer from local to upload
 //to save image in disk storage
@@ -28,24 +28,25 @@ function fileFilter (req, file, cb) {
 const upload = multer ({storage:storage,fileFilter}).single('receipt');
 app.get('/home',async(req,res)=>{
   let userID=req.header('userID')
-  let token=req.header('token')
-  jwt.verify(token ,'patient',async(err,decoded)=>{
-    if(err){
-      res.json({err});
-    } else{
-         req.userID = decoded.userID;
+  //let token=req.header('token')
+  // jwt.verify(token ,'patient',async(err,decoded)=>{
+  //   if(err){
+  //     res.json({err});
+  //   } else{
+  //        req.userID = decoded.userID;
      
     let images=await diagonsticForm.find({ userID})
     res.json(images)
-    }
-  })
+   // }
+ // })
  
 })
 
-app.post('/upload',upload, auth ,async(req,res)=>{
+app.post('/upload',upload,async(req,res)=>{
 
  //console.log(req.file)
- const{userID,token}=req.body
+// const{userID,token}=req.body
+   const{userID}=req.body
  try{
   await diagonsticForm.insertMany({
     path:req.file.path,
